50b4ed3efd53b076de07319f4b604e404c55fe96
raff-k <qo23hel@ads.uni-jena.de>
2020-05-11 18:10:54 +0200

    update citation file

---
+++ inst/CITATION.R
@@ -0,0 +1,14 @@
+citHeader("To cite package 'RainSlide' in publications use:")
+citEntry(entry = "Manual",
+         title = "RainSlide: Rainfall-induced landslide analysis tools",
+         author = personList(as.person("Raphael Knevels")),
+         year = "2020",
+         note = "R package version 0.0.0-9000",
+         url = "https://github.com/raff-k/RainSlide",
+         header = "To cite the 'RainSlide' package use:",
+         textVersion =
+           paste("Knevels, R. 2020",
+                 "RainSlide: Rainfall-induced landslide analysis tools.",
+                 "R package version 0.0.0-9000.",
+                 "https://github.com/raff-k/RainSlide")
+)
